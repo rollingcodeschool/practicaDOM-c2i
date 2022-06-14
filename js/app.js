c3e7e2f5bdaaad69fa207bad8f02665c5f50cf05
Emilse Arias <emilsearias@gmail.com>
2022-06-14 13:15:32 -0300

    agregamos funcionalidad para eliminar el parrafo creado

--- js/app.js
+++ js/app.js
@@ -14,20 +14,37 @@ function cambiarTitulo() {
 
 function verMas(){
     console.log('Desde la funcion verMas');
-    // crear el parrafo
-    let parrafoNuevo = document.createElement('p'); //<p></p>
-    console.log(parrafoNuevo);
-    // agregar texto al parrafo
-    parrafoNuevo.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora repellendus consequuntur dolor aut sunt, vero laboriosam ipsa? Alias tenetur quos eligendi dicta consectetur doloribus inventore nobis, aperiam, eum nulla error. Labore aut ut hic. Dolores ut voluptates quos consectetur! Accusantium sed deleniti soluta neque excepturi est sint quidem eaque, officiis, molestiae assumenda. Magni aliquid unde atque modi! Eligendi dolorum enim rem voluptatibus! Assumenda modi cumque in laboriosam provident laborum, maiores ea et fugiat iste dolor voluptatibus nihil beatae consectetur, impedit non fuga, temporibus ex eius dignissimos quisquam eaque corporis id exercitationem? Architecto natus a rerum distinctio soluta maxime, ut ipsa velit ab nulla, aspernatur est aliquam eveniet, quae quis deleniti nam porro quos eaque non adipisci enim? Ipsum voluptas esse alias reprehenderit. Molestiae atque tempore, dolorum accusamus reiciendis quidem dicta voluptas eius adipisci cumque molestias magnam. Architecto perspiciatis illo sunt dolores ea in nostrum enim deleniti quas natus! Nostrum, illo!';
-    parrafoNuevo.className = 'lead';
-
+     // traer el boton
+    let btnVerMas = document.querySelector('#btnVerMas');
     // insertar ese parrafo en el html
     let articulosPadres = document.getElementsByTagName('article');
-    console.log(articulosPadres[1]);
-    articulosPadres[1].appendChild(parrafoNuevo);
-    // traer el boton
-    let btnVerMas = document.querySelector('#btnVerMas');
-    console.log(btnVerMas);
-    btnVerMas.innerHTML = 'Ocultar';
-    btnVerMas.className = 'btn btn-danger';
+
+    if(btnVerMas.className === 'btn btn-primary'){
+        // crear el parrafo
+        let parrafoNuevo = document.createElement('p'); //<p></p>
+        console.log(parrafoNuevo);
+        // agregar texto al parrafo
+        parrafoNuevo.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora repellendus consequuntur dolor aut sunt, vero laboriosam ipsa? Alias tenetur quos eligendi dicta consectetur doloribus inventore nobis, aperiam, eum nulla error. Labore aut ut hic. Dolores ut voluptates quos consectetur! Accusantium sed deleniti soluta neque excepturi est sint quidem eaque, officiis, molestiae assumenda. Magni aliquid unde atque modi! Eligendi dolorum enim rem voluptatibus! Assumenda modi cumque in laboriosam provident laborum, maiores ea et fugiat iste dolor voluptatibus nihil beatae consectetur, impedit non fuga, temporibus ex eius dignissimos quisquam eaque corporis id exercitationem? Architecto natus a rerum distinctio soluta maxime, ut ipsa velit ab nulla, aspernatur est aliquam eveniet, quae quis deleniti nam porro quos eaque non adipisci enim? Ipsum voluptas esse alias reprehenderit. Molestiae atque tempore, dolorum accusamus reiciendis quidem dicta voluptas eius adipisci cumque molestias magnam. Architecto perspiciatis illo sunt dolores ea in nostrum enim deleniti quas natus! Nostrum, illo!';
+        parrafoNuevo.className = 'lead';
+    
+        console.log(articulosPadres[1]);
+        articulosPadres[1].appendChild(parrafoNuevo);
+        
+        // cambiar estetica del boton
+        console.log(btnVerMas);
+        btnVerMas.innerHTML = 'Ocultar';
+        btnVerMas.className = 'btn btn-danger';
+    }else{
+        console.log('la clase del boton es la danger');
+        // eliminar el nodo nuevo 'parrafo'
+        console.log(articulosPadres[1].hasChildNodes());
+        console.log(articulosPadres[1].children);
+        // eliminamos efectivamente el nodo
+        articulosPadres[1].removeChild(articulosPadres[1].children[2]);
+        // cambiar el texto y la estetica del boton
+        btnVerMas.innerHTML = 'Ver mas ...';
+        btnVerMas.className = 'btn btn-primary';
+    }
+   
+   
 }
